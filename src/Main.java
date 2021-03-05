851efaaf30b9f36e70a0b8f7adb98522dcb2e0e2
TAKAYAMAN2180 <sirokurodog_2180@yahoo.co.jp>
2021-03-05 23:31:43 +0900

    Accomplished

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+        int countSelf = 0;
+        int countCompanion = 0;
+        System.out.println("出す手を選んで数字を入力してね。相手より先に３回勝とう！\n1:グー　2：チョキ　3：パー");
+        while (countCompanion < 3 && countSelf < 3) {
+            int handSelf = new java.util.Scanner(System.in).nextInt();
+            int handCompanion = new java.util.Random().nextInt(3) + 1;
+            //1:グー　2：チョキ　3：パー
+            int victory = handSelf - handCompanion;
+            switch (handCompanion) {
+                case 1:
+                    System.out.print("相手はグーを出しました。");
+                    break;
+                case 2:
+                    System.out.print("相手はチョキを出しました。");
+                    break;
+                case 3:
+                    System.out.print("相手はパーを出しました。");
+                    break;
+            }
+            switch (victory) {
+                case -1:
+                case 2:
+                    System.out.println("あなたの勝ち！");
+                    countSelf++;
+                    break;
+                case 0:
+                    System.out.println("あいこ！もう一回！");
+                    break;
+                case -2:
+                case 1:
+                    System.out.println("あなたの負け！");
+                    countCompanion++;
+                    break;
+            }
+            System.out.println("（自分の勝利数" + countSelf + "回/相手の勝利数" + countCompanion + "回）");
+
+        }
+        if (countSelf == 3) {
+            System.out.println("あなたの優勝です！！！");
+        } else {
+            System.out.println("相手の優勝です…");
+        }
+    }
+}
